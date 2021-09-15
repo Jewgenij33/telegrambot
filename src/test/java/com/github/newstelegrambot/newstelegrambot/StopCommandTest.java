d9710a587036e7d1f851849502f5a7d5875fb020
Jewgenij33 <73817641+Jewgenij33@users.noreply.github.com>
2021-09-16 01:07:55 +0200

    Jrtb 3 (#22)
    * NTB-3: implement Command pattern for telegram bot. Create command "Start", "Stop", "Unknown command", "No command", "help". Create CommandContainer and rewrite NewTelegramBot.class
    
    * NTB-3: implement Command pattern for telegram bot. Create command "Start", "Stop", "Unknown command", "No command", "help". Create CommandContainer and rewrite NewTelegramBot.class
    create Command Tests

---
+++ src/test/java/com/github/newstelegrambot/newstelegrambot/StopCommandTest.java
@@ -0,0 +1,27 @@
+package com.github.newstelegrambot.newstelegrambot;
+
+import com.github.newstelegrambot.newstelegrambot.command.Command;
+import com.github.newstelegrambot.newstelegrambot.command.StopCommand;
+import org.junit.jupiter.api.DisplayName;
+
+import static com.github.newstelegrambot.newstelegrambot.command.CommandName.STOP;
+import static com.github.newstelegrambot.newstelegrambot.command.StopCommand.STOP_MESSAGE;
+
+@DisplayName("Unit-level testing for StopCommand")
+public class StopCommandTest extends AbstractCommandTest {
+
+    @Override
+    String getCommandName() {
+        return STOP.getCommandName();
+    }
+
+    @Override
+    String getCommandMessage() {
+        return STOP_MESSAGE;
+    }
+
+    @Override
+    Command getCommand() {
+        return new StopCommand(sendBotMessageService);
+    }
+}
