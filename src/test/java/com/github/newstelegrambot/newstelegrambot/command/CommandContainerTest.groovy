8b33db47195b775d4ec59de6876cb9acea52f24c
ko <1111>
2022-06-17 23:20:34 +0200

    - create integration test - fixed bugs in tests - fixed bugs in mvn

--- src/test/java/com/github/newstelegrambot/newstelegrambot/command/CommandContainerTest.groovy
+++ src/test/java/com/github/newstelegrambot/newstelegrambot/command/CommandContainerTest.groovy
@@ -2,19 +2,17 @@ package com.github.newstelegrambot.newstelegrambot.command
 
 import com.github.newstelegrambot.newstelegrambot.service.SendBotMessageService
 import com.github.newstelegrambot.newstelegrambot.service.TelegramUserService
-import com.github.newstelegrambot.newstelegrambot.service.TelegramUserServiceImpl
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
 
 @DisplayName("Unit-testing for CommandContainer")
 @ExtendWith(MockitoExtension.class)
-class CommandContainerTest {
+public class CommandContainerTest {
 
     private CommandContainer commandContainer;
 
@@ -23,7 +21,7 @@ class CommandContainerTest {
     SendBotMessageService sendBotMessageService;
 
     @BeforeEach
-    void init() {
+    public void init() {
 
 //        TelegramUserService telegramUserService = Mockito.mock(TelegramUserService.class);
 //        SendBotMessageService sendBotMessageService = Mockito.mock(SendBotMessageService.class);
@@ -32,7 +30,7 @@ class CommandContainerTest {
     }
 
     @Test
-    void shouldGetRunAllExistCommands() {
+    public void shouldGetRunAllExistCommands() {
 
         List<Command> commands = new ArrayList<Command>();
         for (Command command : commands) {
@@ -45,7 +43,7 @@ class CommandContainerTest {
 
         String unknownCommand = "/asdgsgda"
         Command command = commandContainer.retrieveCommand(unknownCommand);
-        Assertions.assertEquals(UnknownCommand.class, command);
+        Assertions.assertEquals(UnknownCommand.class, command.getClass());
 
     }
 }
