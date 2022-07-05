c9b064f62f2130b3daaa9856f12afec8e3b88ad8
ko <1111>
2022-07-05 06:56:39 +0200

    remove annotation @Profile in main class NewsTelegramBotApplication.java for successful build

--- src/main/java/com/github/newstelegrambot/newstelegrambot/NewsTelegramBotApplication.java
+++ src/main/java/com/github/newstelegrambot/newstelegrambot/NewsTelegramBotApplication.java
@@ -2,9 +2,8 @@ package com.github.newstelegrambot.newstelegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
-@Profile("test")
+//@Profile("test")
 @SpringBootApplication
 public class NewsTelegramBotApplication {
 
